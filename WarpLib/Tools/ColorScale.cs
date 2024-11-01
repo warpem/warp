ead6d45b0ba6bb9309e15d45fe1679e0da391264
Dimitry Tegunov <tegunov@gmail.com>
2024-11-01 15:18:00 -0700

    Quantize ColorScale to avoid linear interpolation

--- WarpLib/Tools/ColorScale.cs
+++ WarpLib/Tools/ColorScale.cs
@@ -9,21 +9,32 @@ namespace Warp.Tools
 {
     public class ColorScale
     {
-        float4[] Stops;
+        int QuantizationSteps;
+        int4[] Stops;
 
-        public ColorScale(float4[] stops)
+        public ColorScale(float4[] stops, int quantizationSteps)
         {
-            Stops = stops;
+            QuantizationSteps = quantizationSteps;
+            Stops = new int4[QuantizationSteps];
+
+            for (int i = 0; i < QuantizationSteps; i++)
+                Stops[i] = GetColor(stops, (float)i / (QuantizationSteps - 1));
         }
 
-        public float4 GetColor(float v)
+        private int4 GetColor(float4[] stops, float v)
         {
-            v *= Stops.Length - 1;
+            v *= stops.Length - 1;
 
-            float4 C1 = Stops[Math.Min(Stops.Length - 1, Math.Max((int)v, 0))];
-            float4 C2 = Stops[Math.Min(Stops.Length - 1, Math.Max((int)v + 1, 0))];
+            float4 C1 = stops[Math.Min(stops.Length - 1, Math.Max((int)v, 0))];
+            float4 C2 = stops[Math.Min(stops.Length - 1, Math.Max((int)v + 1, 0))];
 
-            return float4.Lerp(C1, C2, v - (int)v);
+            float4 Interp = float4.Lerp(C1, C2, v - (int)v);
+            return new int4((int)Math.Clamp(Interp.X * 255, 0, 255),
+                            (int)Math.Clamp(Interp.Y * 255, 0, 255),
+                            (int)Math.Clamp(Interp.Z * 255, 0, 255),
+                            (int)Math.Clamp(Interp.W * 255, 0, 255));
         }
+
+        public int4 GetColor(float v) => Stops[(int)(Math.Clamp(v, 0, 1) * (QuantizationSteps - 1))];
     }
 }
