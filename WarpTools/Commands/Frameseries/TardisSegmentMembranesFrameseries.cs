8f34795d8af501a57e907037cc484080c0303cbb
Alister Burt <alisterburt@gmail.com>
2025-02-13 18:57:02 -0800

    tidy up

--- WarpTools/Commands/Frameseries/TardisSegmentMembranesFrameseries.cs
+++ WarpTools/Commands/Frameseries/TardisSegmentMembranesFrameseries.cs
@@ -30,46 +30,12 @@ namespace WarpTools.Commands
             OptionsWarp Options = CLI.Options;
             ProcessingOptionsTardisSegmentMembranes2D OptionsTardis = Options.GetProcessingTardisSegmentMembranes2D();
 
-            #region Set options
-            //
-            // Options.Movement.RangeMin = (decimal)CLI.RangeMin;
-            // Options.Movement.RangeMax = (decimal)CLI.RangeMax;
-            // Options.Movement.Bfactor = (decimal)CLI.Bfactor;
-            //
-            // if (!string.IsNullOrEmpty(CLI.GridDims))
-            // {
-            //     try
-            //     {
-            //         var Dims = CLI.GridDims.Split('x');
-            //
-            //         Options.Grids.MovementX = int.Parse(Dims[0]);
-            //         Options.Grids.MovementY = int.Parse(Dims[1]);
-            //         Options.Grids.MovementZ = int.Parse(Dims[2]);
-            //     }
-            //     catch
-            //     {
-            //         throw new Exception("Grid dimensions must be specified as XxYxZ, e.g. 5x5x40, or left empty for auto");
-            //     }
-            // }
-            // else
-            // {
-            //     Options.Grids.MovementX = 0;
-            //     Options.Grids.MovementY = 0;
-            //     Options.Grids.MovementZ = 0;
-            // }
-            //
-            // Options.Export.DoAverage = CLI.Averages;
-            // Options.Export.DoDenoise = CLI.AverageHalves;
-            // Options.Export.SkipFirstN = CLI.SkipFirst;
-            // Options.Export.SkipLastN = CLI.SkipLast;
-
-            #endregion
-
             WorkerWrapper[] Workers = CLI.GetWorkers();
 
             IterateOverItemsBatched(Workers, CLI, (worker, movies) =>
             {
-                worker.TardisSegmentMembranes2D(movies.Select(m => m.Path).ToArray(), OptionsTardis);
+                var paths = movies.Select(m => m.Path).ToArray();
+                worker.TardisSegmentMembranes2D(paths, OptionsTardis);
             });
 
             Console.Write("Saying goodbye to all workers...");
