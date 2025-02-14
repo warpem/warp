3d503cd3b0011d43dc7c9d6f60635886b1f41dc0
Alister Burt <alisterburt@gmail.com>
2025-02-14 14:45:20 -0800

    correct batch size usage

--- WarpTools/Commands/Frameseries/TardisSegmentMembranesFrameseries.cs
+++ WarpTools/Commands/Frameseries/TardisSegmentMembranesFrameseries.cs
@@ -40,7 +40,8 @@ namespace WarpTools.Commands
                     var paths = movies.Select(m => m.Path).ToArray();
                     worker.TardisSegmentMembranes2D(paths, OptionsTardis);
                 },
-                getBatch: (start, end) => CLI.InputSeries[start..end]
+                getBatch: (start, end) => CLI.InputSeries[start..end],
+                getBatchSize: batch => batch.Length
             );
 
             Console.Write("Saying goodbye to all workers...");
