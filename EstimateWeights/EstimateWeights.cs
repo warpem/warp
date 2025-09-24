87aa884cacad44930a4de4049cc690d682238d11
Dimitry Tegunov <tegunov@gmail.com>
2025-09-24 14:26:20 -0700

    Hundredth time's the charm?

--- EstimateWeights/EstimateWeights.cs
+++ EstimateWeights/EstimateWeights.cs
@@ -320,9 +320,9 @@ namespace EstimateWeights
                             {
                                 if (Options.ResolveItems && Options.ResolveFrames)
                                 {
-                                    AllAB.Add(FSCData[z * 3 + 0]);
-                                    AllA2.Add(FSCData[z * 3 + 1]);
-                                    AllB2.Add(FSCData[z * 3 + 2]);
+                                    AllAB.Add(FSCData[z * 3 + 0].ToArray());
+                                    AllA2.Add(FSCData[z * 3 + 1].ToArray());
+                                    AllB2.Add(FSCData[z * 3 + 2].ToArray());
                                 }
                                 else
                                 {
